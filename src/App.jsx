5341e8f98b6ff03c76cac121c871892c796ce842
Marlleyck Nathan <marlleyck@hotmail.com>
2022-07-27 21:32:04 -0300

    implementado requisicao com api

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import Home from './components/Home';
 
 function App() {
 
+  const [inputCity, setInputCity] = useState('')
   const [city, setCity] = useState()
   const [desc, setDesc] = useState()
   const [temp, setTemp] = useState()
+
 
   const url = 
   'https://api.openweathermap.org/data/2.5/weather?q='
@@ -17,12 +19,12 @@ function App() {
   useEffect(() => {
 
     const fetchApi = async () => {
-      const {data} = await axios.get(`${url}Caraúbas&appid=2bdef8885b55003598b381a2dc578987&lang=pt_br&units=metric`)
+      const {data} = await axios.get(`${url}Brasília&appid=2bdef8885b55003598b381a2dc578987&lang=pt_br&units=metric`)
 
       console.log(data)
       setCity(data.name)
+      setTemp(Math.ceil(data.main.temp))
       setDesc(data.weather[0].description)
-      setTemp(data.main.temp)
     }
 
 
@@ -36,8 +38,12 @@ function App() {
         <Route path='/' exact 
         element={<Home 
         city={city}
+        setCity={setCity}
         desc={desc}
-        temp={temp} />} />
+        setDesc={setDesc}
+        temp={temp}
+        setTemp={setTemp}
+        url={url} />} />
       </Routes>
     </Router>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
+import {FiSearch} from 'react-icons/fi'
+import lua_nublado from '../img/lua_nublado.png'
 
 import './Home.css'
 
-const Home = ({city, desc, temp}) => {
+const Home = ({city, desc, temp, setCity, setDesc, setTemp, url}) => {
+
+    const [input, setInput] = useState()
+
+    const handleClickInput = async (e) => {
+        const {data} = await axios.get(`${url}${input}&appid=2bdef8885b55003598b381a2dc578987&lang=pt_br&units=metric`)
+
+        setCity(data.name)
+        setTemp(Math.ceil(data.main.temp))
+        setDesc(data.weather[0].description)
+        setInput('')
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            handleClickInput()
+        }
+    }
 
     return ( 
         <div className='container'>
             <main className='main'>
                 <div className='cont_details'>
-                    <h3>{city}</h3>
+                    <h2 className='city'>{city}</h2>
 
-                    <div className='contain_temp'>
-                        <h1>{temp}</h1>
-                        <img src="" alt="" />
+                    <div className='cont_temp'>
+                        <img src={lua_nublado} alt="noite" className='imagem' />
+                        <h1 className='temp'>{temp}º</h1>
+                        <h2 className='desc'>{desc}</h2>
                     </div>
-                    
-                    <h4>{desc}</h4>
                 </div>
 
-                <input type="text" />
+                <div className='cont_search'>
+                    <input type="text" 
+                    placeholder='Digite uma cidade'
+                    onChange={(e) => setInput(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                    value={input} />
+
+                    <FiSearch size={30} color='#000' id='search'
+                    onClick={handleClickInput} />
+                </div>
             </main>
 
             <div className="background"></div>
