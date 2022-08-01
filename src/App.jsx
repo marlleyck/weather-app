0eecf4e1da006df5c55116f653b91e52714c3c79
Marlleyck Nathan <marlleyck@hotmail.com>
2022-08-01 19:26:34 -0300

    add funcao de trocar img

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import axios from 'axios';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { useEffect, useState } from 'react';
+import { useEffect, useState} from 'react';
 
 import Home from './components/Home';
 
-
 function App() {
 
+  
   const [city, setCity] = useState()
   const [desc, setDesc] = useState()
   const [temp, setTemp] = useState()
-
 
   const url = 
   'https://api.openweathermap.org/data/2.5/weather?q='
@@ -18,9 +17,9 @@ function App() {
   useEffect(() => {
 
     const fetchApi = async () => {
+
       const {data} = await axios.get(`${url}Brasília&appid=2bdef8885b55003598b381a2dc578987&lang=pt_br&units=metric`)
 
-      console.log(data)
       setCity(data.name)
       setTemp(Math.ceil(data.main.temp))
       setDesc(data.weather[0].description)
@@ -33,17 +32,17 @@ function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path='/' exact 
-        element={<Home 
-        city={city}
-        setCity={setCity}
-        desc={desc}
-        setDesc={setDesc}
-        temp={temp}
-        setTemp={setTemp}
-        url={url} />} />
-      </Routes>
+        <Routes>
+          <Route path='/' exact 
+          element={<Home 
+          city={city}
+          setCity={setCity}
+          desc={desc}
+          setDesc={setDesc}
+          temp={temp}
+          setTemp={setTemp}
+          url={url} />} />
+        </Routes>
     </Router>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,18 @@
 import React, { useState } from 'react';
 import axios from 'axios'
 import {FiSearch} from 'react-icons/fi'
+
+import lua_chuva from '../img/lua_chuva.png'
 import lua_nublado from '../img/lua_nublado.png'
+import lua from '../img/lua.png'
+import sol_chuva from '../img/sol_chuva.png'
+import sol_nublado from '../img/sol_nublado.png'
+import sol from '../img/sol.png'
+import sol_tempestade from '../img/sol_tempestade.png'
+import lua_tempestade from '../img/lua_tempestade.png'
+import sol_neve from '../img/sol_neve.png'
+import lua_neve from '../img/lua_neve.png'
+import nuvem from '../img/nuvem.png'
 
 import './Home.css'
 
@@ -11,15 +22,58 @@ const Home = ({city, desc, temp, setCity, setDesc, setTemp, url}) => {
 
     const [country, setCountry] = useState('BR')
 
+    const [imag, setImag] = useState(nuvem)
+
+    const testVaria = (data) => {
+        const hour = new Date().getHours()
+
+        if (hour >= 18) {
+            if (data == 'Thunderstorm') {
+                setImag(lua_tempestade)
+            } else if (data == 'Clouds') {
+                setImag(lua_nublado)
+            } else if (data == 'Drizzle') {
+                setImag(lua_chuva)
+            } else if (data == 'Rain') {
+                setImag(lua_chuva)
+            } else if (data == 'Snow') {
+                setImag(lua_neve)
+            } else if (data == 'Atmosphere') {
+                setImag(lua)
+            } else if (data == 'Clear') {
+                setImag(lua)
+            }
+        } else if (hour >= 5) {
+            if (data == 'Thunderstorm') {
+                setImag(sol_tempestade)
+            } else if (data == 'Clouds') {
+                setImag(sol_nublado)
+            } else if (data == 'Drizzle') {
+                setImag(sol_chuva)
+            } else if (data == 'Rain') {
+                setImag(sol_chuva)
+            } else if (data == 'Snow') {
+                setImag(sol_neve)
+            } else if (data == 'Atmosphere') {
+                setImag(sol)
+            } else if (data == 'Clear') {
+                setImag(sol)
+            }
+        }
+    }
+
     const handleClickInput = async () => {
         if (input == '' || country == '') {
             alert('Complete todos os dados para prosseguir!')
         } else {
             const {data} = await axios.get(`${url}${input},${country}&appid=2bdef8885b55003598b381a2dc578987&lang=pt_br&units=metric`)
 
+            console.log(data)
             setCity(data.name)
             setTemp(Math.ceil(data.main.temp))
             setDesc(data.weather[0].description)
+
+            testVaria(data.weather[0].main)
             setInput('')
         }
 
@@ -38,7 +92,7 @@ const Home = ({city, desc, temp, setCity, setDesc, setTemp, url}) => {
                     <h2 className='city'>{city}</h2>
 
                     <div className='cont_temp'>
-                        <img src={lua_nublado} alt="noite" className='imagem' />
+                        <img src={imag} alt="noite" className='imagem' />
                         <h1 className='temp'>{temp}º</h1>
                         <h2 className='desc'>{desc}</h2>
                     </div>
